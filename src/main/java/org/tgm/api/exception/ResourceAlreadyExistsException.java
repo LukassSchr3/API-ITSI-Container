fadd8b49532182e312196906025d4d2fa0708072
Lukas <lschrenk@student.tgm.ac.at>
2025-11-29 15:26:04 +0100

    Delete src

--- src/main/java/org/tgm/api/exception/ResourceAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package org.tgm.api.exception;
-
-public class ResourceAlreadyExistsException extends RuntimeException {
-    public ResourceAlreadyExistsException(String message) {
-        super(message);
-    }
-}
-
